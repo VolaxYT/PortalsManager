35fae9728db9d7b0c4e39983a2a85c1dfa0f9e11
Volax <47873891+VolaxYT@users.noreply.github.com>
2020-06-11 21:07:11 +0200

    1.0.03a

--- src/fr/volax/portal/PortalsCommands.java
+++ src/fr/volax/portal/PortalsCommands.java
@@ -27,8 +27,7 @@ public class PortalsCommands implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if(args.length != 1) { helpMessage(sender); return false; }
-        if(args[0].equalsIgnoreCase("reload")){
-            if(sender.isOp() || sender.hasPermission(ConfigBuilder.getString("permissions.reload"))){
+        if(args[0].equalsIgnoreCase("reload") && sender.isOp() || sender.hasPermission(ConfigBuilder.getString("permissions.reload"))){
                 MainPortal.getMain().reloadConfig();
                 System.out.println(ConfigBuilder.getString("messages.config-reload"));
                 return false;
